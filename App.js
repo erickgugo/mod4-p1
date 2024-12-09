1c55477e4397dc9854ea7edfdffef4b505041ab2
Erick Gutierrez <erick.gutierrez@axsbolivia.net>
2024-12-09 16:05:35 -0400

    Se inicia proyecto

--- App.js
+++ App.js
@@ -4,8 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>Hola Univalle</Text>
+      <Text>Bienvenidos al modulo 4</Text>
     </View>
   );
 }
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
