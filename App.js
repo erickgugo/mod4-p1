b9d25468a41e3ed32718159425d0578ac32fca88
Erick Gutierrez <erick.gutierrez@axsbolivia.net>
2024-12-09 16:56:45 -0400

    Se agrega un boton touch con alert

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 import {StatusBar} from 'expo-status-bar';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Alert, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React from "react";
+import logoUnivalle from './assets/univalle1.png';
 
 export default function App() {
     return (
         <View style={styles.container}>
             <StatusBar style="auto"/>
-            <Image
-                source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Univalle_bol_cbb_logo.png'}}
-                   style={styles.logo}
-            />
+            {/*<Image*/}
+            {/*    source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Univalle_bol_cbb_logo.png'}}*/}
+            {/*       style={styles.logo}*/}
+            {/*/>*/}
+            <Image source={logoUnivalle} style={styles.logo2} />
 
             <Text style={styles.titulo}>Hola Univalle</Text>
             <Text style={styles.subtitulo}>Bienvenidos al modulo 4</Text>
+            <TouchableOpacity style={styles.boton2}
+            onPress={() => Alert.alert("Hola Univalle")}>
+                <Text style={styles.textoBoton}>Haz click aqui</Text>
+            </TouchableOpacity>
         </View>
     );
 }
@@ -20,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'pink',
+        //backgroundColor: 'pink',
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
@@ -33,6 +39,21 @@ const styles = StyleSheet.create({
         alignSelf: 'center'
     },
     logo: {
-        height: 200, width: 200
-    }
+        height: 100,
+        width: 100,
+    },
+    logo2: {
+        height: 200,
+        width: 200,
+        borderRadius: 100
+    },
+    boton2: {
+      backgroundColor: 'cyan',
+        margin: 20,
+        padding:15,
+    },
+    textoBoton: {
+        fontSize: 20,
+        color: 'white',
+    },
 });
